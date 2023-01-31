8155815a6b5fd97a7b189f2f36d93b47e0289bb3
peterkurzzz <peterkurzz@bk.ru>
2023-01-31 20:32:14 +0300

    Задание 21.8.2

---
+++ rectangle.py
@@ -0,0 +1,28 @@
+# К ЗАДАНИЮ 21.8.2
+import math
+
+
+class Rectangle:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def get_area(self):
+        return self.a * self.b
+
+
+class Square:
+    def __init__(self, a):
+        self.a = a
+
+    def get_area_square(self):
+        return self.a ** 2
+
+
+class Circle:
+    def __init__(self, r):
+        self.r = r
+        self.pi = math.pi
+
+    def get_area_circle(self):
+        return self.pi * self.r ** 2
